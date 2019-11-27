a1daeb0cc35a157cc13118c8ee161461e314d600
gangtiexia <2656383194@qq.com>
2019-11-27 11:37:30 +0800

    工厂方法的代码的编写

--- page/page_factroy.py
+++ page/page_factroy.py
@@ -6,12 +6,18 @@ from page.login_page import LoginPage
 from page.mine_page import MinePage
 
 
-class PageFactroy(object):
-    def __init__(self,driver):
-        self.driver=driver
+class PageFactory(object):
+    def __init__(self, driver):
+        self.driver = driver
+    @property
     def home_page(self):
-       return  HomePage(self.driver)
+        """主页页面"""
+        return HomePage(self.driver)
+    @property
     def mine_page(self):
+        """我的页面"""
         return MinePage(self.driver)
+    @property
     def login_page(self):
+        """登录页面"""
         return LoginPage(self.driver)
