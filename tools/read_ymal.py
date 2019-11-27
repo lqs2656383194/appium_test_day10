21de507ec8c107e0d575457f4e8c6ac1340b1408
gangtiexia <2656383194@qq.com>
2019-11-27 18:27:17 +0800

    数据源解析方法的修改

--- tools/read_ymal.py
+++ tools/read_ymal.py
@@ -1,14 +1,17 @@
 import yaml
+
+
 def bulid_login_data():
-    with open('../data/login_data.yaml',encoding='ytf-8') as f:
-        data=yaml.safe_load(f)
-        data_list=list()
+    with open('../data/login_data.yaml', encoding='utf-8') as f:
+        data = yaml.safe_load(f)
+        data_list = list()
         for i in data.values():
             data_list.append((i.get('name'),
-                  i.get('pwd'),
-                  i.get('expect'),
-                  i.get('is_success')))
+                              i.get('pwd'),
+                              i.get('expect'),
+                              i.get('is_success')))
         print(data_list)
         return data_list
 
 
+bulid_login_data()
